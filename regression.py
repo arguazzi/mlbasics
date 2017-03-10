03a8b8a6aed7f4853197075f904113ef412a481f
Alessandro <arguazzi@gmail.com>
2017-03-10 16:51:48 +0000

    Created tests for logistic and linear regression.

--- regression.py
+++ regression.py
@@ -9,7 +9,11 @@ class GradientDescent:
     def __init__(self, x, y, alpha, toggle, iterations):
         self.x = x
         self.y = y
-        self.alpha = alpha
+
+        if toggle == "linreg":
+            self.alpha = alpha
+        elif toggle == "logreg":
+            self.alpha = -alpha
         self.toggle = toggle
         self.iterations = iterations
 
@@ -22,13 +26,17 @@ class GradientDescent:
         self.loss = np.zeros([iterations,1])
         self.hypo = np.zeros(self.n)
 
+    @staticmethod
+    def sigmoid(k):
+        return 1 / (1 + np.exp(k))
+
     def hypothesis(self):
         linear = np.dot(self.x, self.theta)
         # Linear case
         if self.toggle == "linreg":
             self.hypo = linear
         elif self.toggle == "logreg":
-            self.hypo = 1 / (1+np.exp(linear))
+            self.hypo = self.sigmoid(linear)
 
     def batch(self, i):
         self.hypothesis()
@@ -43,21 +51,47 @@ class GradientDescent:
         for i in range(0, self.iterations):
             self.batch(i)
 
-xx = np.ones([100, 2])
-xx[:, 1] = range(0, 100)
-var = 5
-yy = np.dot(xx, [[50], [5]]) + var*np.random.randn(100, 1)
 
-gd = GradientDescent(xx, yy, 0.0001, "linreg", 100000)
+def test_linreg():
+    # Test Linear regression
+    xx = np.ones([100, 2])
+    xx[:, 1] = range(0, 100)
+    var = 5
+    yy = np.dot(xx, [[50], [5]]) + var*np.random.randn(100, 1)
 
-# ls, t = BatchDescent(yy, xx, 0.00001, 100000)
-gd.run()
+    gd = GradientDescent(xx, yy, 0.0001, "linreg", 100000)
 
-plt.plot(gd.loss)
-plt.show()
+    gd.run()
 
-# plt.plot(yy, '.r')
-# plt.plot(np.dot(xx, t), '.b')
-# plt.show()
+    f, axarr = plt.subplots(2, sharex=False)
+    axarr[0].plot(gd.loss)
 
-print(gd.theta)
+    axarr[1].plot(xx[:, 1], yy, '.')
+    axarr[1].plot(xx[:, 1], np.dot(xx, gd.theta), '-')
+    plt.show()
+
+    print(gd.theta)
+
+
+def test_logreg():
+    # Test Logistic regression
+    xx = np.ones([100, 2])
+    xx[:, 1] = range(0, 100)
+    yy = np.zeros([100, 1])
+    yy[70:] = 1
+
+    gd = GradientDescent(xx, yy, 0.01, "logreg", 100000)
+    gd.run()
+
+    f, axarr = plt.subplots(2, sharex=False)
+    axarr[0].plot(gd.loss)
+
+    axarr[1].plot(xx[:,1], yy, '.')
+    axarr[1].plot(xx[:,1], gd.sigmoid(np.dot(xx, gd.theta)), '-')
+    axarr[1].plot(xx[:,1], gd.sigmoid(np.dot(xx, gd.theta))>=0.5, '-')
+
+    plt.show()
+
+    print(gd.theta)
+
+test_logreg()
