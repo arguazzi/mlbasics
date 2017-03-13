ed279d7257e4714afeb9b504528b31cae2e1b7f9
Alessandro <arguazzi@gmail.com>
2017-03-13 20:46:39 +0000

    Added Linear Discriminant Analysis implementation

---
+++ lda.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python2
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class LDA:
+
+    def __init__(self):
+        self.sigma_inv = 0
+        self.const = 0
+        self.coeff = 0
+
+    def train(self, train_input, train_class):
+
+        # Input matrix with values as columns and variables as rows.
+        x = train_input
+        y = train_class
+        m = x.shape[0]
+        n = x.shape[1]
+        k = len(np.unique(train_class))
+        self.const = np.zeros(k)
+        self.coeff = np.zeros([m, k])
+        sigma = np.cov(x)
+
+        sigma_inv = np.linalg.inv(sigma)
+
+        for i in range(0, k):
+            xx = x[:, y == i]
+            p = xx.shape[0]/n
+            mu = xx.mean(axis=1)
+            mut = mu.transpose()
+            self.const[i] = np.dot(mut, np.dot(sigma_inv, mu)) + np.log(p)
+            self.coeff[:, i] = np.dot(sigma_inv, mu)
+
+    def test(self, test_input):
+
+        x_t = test_input.transpose()
+
+        d = self.const - 2*np.dot(x_t, self.coeff)
+
+        return d
+
+
+# Test that it actually works
+A = np.random.multivariate_normal([1, 2], [[3, 1], [2, 1.3]], 100)*1.0
+B = np.random.multivariate_normal([2, 4], [[3, 1.2], [2, 1.2]], 100)*1.0
+X = np.concatenate([A, B], axis=0).transpose()
+Y = np.concatenate([np.zeros(100), np.ones(100)])
+
+plt.scatter(A[:, 1], A[:, 0], marker='*')
+plt.scatter(B[:, 1], B[:, 0], marker='o')
+plt.show()
+
+l = LDA
+l.train(X, Y)
+d = l.test(X)
+
+plt.scatter(d[d[:, 0] > d[:, 1], 0], d[d[:, 0] > d[:, 1], 1], marker='x')
+plt.scatter(d[d[:, 0] < d[:, 1], 0], d[d[:, 0] < d[:, 1], 1], marker='s')
+plt.show()
+
+
+
+
+
+
+
+
+
+
