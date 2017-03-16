098c9299a449287b88b7080c45589ee21a9795d6
Alessandro <arguazzi@gmail.com>
2017-03-16 21:23:38 +0000

    Cleaned up the test component

--- lda.py
+++ lda.py
@@ -41,26 +41,26 @@ class LDA:
 
         return d
 
+if __name__ == '__main__':
+    # Test that it actually works
+    A = np.random.multivariate_normal([1, 2], [[0.3, 0.1], [0.2, 0.3]], 100)*1.0
+    B = np.random.multivariate_normal([2, 4], [[0.3, 0.2], [0.2, 0.2]], 100)*1.0
+    C = np.random.multivariate_normal([0, 0], [[0.3, 0.22], [0.2, 0.2]], 100)*1.0
+    X = np.concatenate([A, B, C], axis=0).transpose()
+    Y = np.concatenate([np.zeros(100), np.ones(100), np.ones(100)*2.0])
 
-# Test that it actually works
-A = np.random.multivariate_normal([1, 2], [[3, 1], [2, 1.3]], 100)*1.0
-B = np.random.multivariate_normal([2, 4], [[3, 1.2], [2, 1.2]], 100)*1.0
-C = np.random.multivariate_normal([0, 0], [[3, 1.2], [2, 1.2]], 100)*1.0
-X = np.concatenate([A, B, C], axis=0).transpose()
-Y = np.concatenate([np.zeros(100), np.ones(100), np.ones(100)*2.0])
+    plt.scatter(A[:, 1], A[:, 0], marker='*')
+    plt.scatter(B[:, 1], B[:, 0], marker='o')
+    plt.scatter(C[:, 1], C[:, 0], marker='s')
+    plt.show()
 
-plt.scatter(A[:, 1], A[:, 0], marker='*')
-plt.scatter(B[:, 1], B[:, 0], marker='o')
-plt.scatter(C[:, 1], C[:, 0], marker='s')
-plt.show()
+    l = LDA()
+    l.train(X, Y)
+    d = l.test(X)
 
-l = LDA()
-l.train(X, Y)
-d = l.test(X)
-
-plt.scatter(d[d[:, 0] > d[:, 1], 1], d[d[:, 0] > d[:, 1], 2], marker='x')
-plt.scatter(d[d[:, 0] < d[:, 1], 1], d[d[:, 0] < d[:, 1], 2], marker='s')
-plt.show()
+    plt.scatter(d[d[:, 0] > d[:, 1], 1], d[d[:, 0] > d[:, 1], 2], marker='x')
+    plt.scatter(d[d[:, 0] < d[:, 1], 1], d[d[:, 0] < d[:, 1], 2], marker='s')
+    plt.show()
 
 
 
